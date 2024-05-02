15230f0e12d1991cd4ae019a84292af2d0daaea7
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-05-02 16:01:54 +0000

    ⚡ added class_with_constructor

--- week_1/classes_and_objects/class_with_constructor/App.py
+++ week_1/classes_and_objects/class_with_constructor/App.py
@@ -1,1 +1,19 @@
-asdfasd
+class Character:
+    def __init__(self, health, attack, defense, name):
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.name = name
+
+    def sayMyName(self):
+        print("My name is", self.name)
+
+
+knight = Character(10, 5, 0, "Chen")
+knight.sayMyName()
+
+mage = Character(0, 5, 10, "Akasha")
+mage.sayMyName()
+
+thief = Character(5, 10, 0, "Gondar")
+thief.sayMyName()
