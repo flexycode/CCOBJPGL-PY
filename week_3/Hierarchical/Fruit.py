08a7d84a5b12d6b5bd2b59a0aa708e271d26206a
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-06-03 15:53:48 +0000

    📓 Add class Fruit.py

---
+++ week_3/Hierarchical/Fruit.py
@@ -0,0 +1,8 @@
+class Fruit:
+    def __init__(self):
+        self.color = None
+        self.taste = None
+
+    def show_color_and_taste(self):
+        print(f"This fruit is color {self.color}")
+        print(f"This fruit is {self.taste}")
